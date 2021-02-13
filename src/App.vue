887ce3b307340da88eea586edc7e600237079110
efrony <efron.vit@gmail.com>
2021-02-13 21:33:25 +0300

    styles

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,81 @@
   <router-view/>
 </template>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  text-align: left;
+  color: #574C50;
 }
 
-#nav {
+h1 {
+  font-size: 15px;
+  font-weight: 700;
+}
+
+h5 {
+  font-size: 11px;
+  font-weight: 500;
+  margin: 5px;
+}
+
+.first-font { 
+    font-size: 10px;
+}
+
+.second-font {
+    font-size: 10px;
+    color: gray;
+}
+
+section {
+  margin-bottom: 25px;
+}
+
+select,
+input[type="text"] {
+  padding: 5px;
+  border: 1px solid lightgray;
+  width: 180px;
+}
+
+input[type="radio"] {
+  color: red;
+  line-height: 35px;
+}
+
+textarea {
+  padding: 5px;
+  border: 1px solid lightgray;
+}
+
+label {
+  position: relative;
+  padding-left: 25px;
+  line-height: 28px;
+  input {
+    transform: scale(1.3);
+    position: absolute;
+    left: -2px;
+    top: -4px
+  }
+}
+
+
+button {
+  text-transform: uppercase;
+  border-radius: 3%;
+  border: none;
+  background-color: #FF9767;
+  color: white;
+  font-size: 10px;
+  font-weight: 500;
+  padding: 9px 12px 7px 12px;
+}
+
+/* #nav {
   padding: 30px;
 }
 
@@ -26,5 +91,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,39 +2,84 @@
   <div class="form">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <h1>Форма подачи заявки в отдел сервиса и качества</h1>
-      <h5>Ваш филиал <span>*</span></h5>
-      <select name="city">
-        <option value="none" disabled selected>Выберите город</option>
-        <option value="Москва">Москва</option>
-        <option value="Санкт-Петербург">Санкт-Петербург</option>
-        <option value="Владивосток">Владивосток</option>
-        <option value="Екатеринбург">Екатеринбург</option>
+    <h5>Ваш филиал <span>*</span></h5>
+    <section>
+      <select class="second-font">
+        <option value="null" disabled selected>Выберите город</option>
+        <option v-for="city in cities" :value="city.id" :key="city.id">{{ city.title }}</option>
       </select>
-      <p><input type="checkbox" name="online-tag" value="online" />Онлайн</p>
+      <br />
+      <label class="first-font"
+        ><input type="checkbox" name="online-tag" value="online" />Онлайн</label
+      >
+    </section>
 
+    <section>
       <h5>Тема обращения <span>*</span></h5>
-      <label><input type="radio" checked name="subject" /> Не доволен качеством услуг</label><br>
-      <label><input type="radio" name="subject" /> Расторжение договора</label><br>
-      <label><input type="radio" name="subject" /> Не приходит письмо активации на почту</label><br>
-      <label><input type="radio" name="subject" /> Не работает личный кабинет</label><br>
-      <input type="text" name="5" placeholder="Другое" />
+      <label class="first-font"
+        ><input type="radio" checked name="subject" /> Не доволен качеством
+        услуг</label
+      ><br />
+      <label class="first-font"
+        ><input type="radio" name="subject" /> Расторжение договора</label
+      ><br />
+      <label class="first-font"
+        ><input type="radio" name="subject" /> Не приходит письмо активации на
+        почту</label
+      ><br />
+      <label class="first-font"
+        ><input type="radio" name="subject" /> Не работает личный кабинет</label
+      ><br />
+      <input type="text" name="5" placeholder="Другое" class="second-font" />
+    </section>
 
+    <section>
       <h5>Описание проблемы <span>*</span></h5>
-      <textarea name="" id="" cols="90" rows="5" placeholder="Введите текст ..."></textarea>
+      <textarea
+        name=""
+        id=""
+        cols="90"
+        rows="5"
+        placeholder="Введите текст"
+        class="first-font"
+      ></textarea>
+    </section>
 
-      <HelloWorld msg="Welcome to Your Vue.js App" />
+    <section>
+      <h5>Загрузка документов</h5>
+      <p class="second-font">
+        Приложите, пожалуйста, полноэкранный скриншот.
+        <br />
+        Это поможет быстрее решить проблему.
+      </p>
+      <p>
+        <input type="file" name="screenshot" class="first-font" />
+      </p>
+    </section>
 
+    <section>
+      <button disabled>Отправить</button>
+    </section>
+
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import SelectCustom from "@/components/SelectCustom.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  // components: {
+  //   SelectCustom,
+  // },
+  
+  computed: {
+        cities() {
+          return [{"id":"1","title":"Москва"},{"id":"2","title":"Санкт-Петербург"},{"id":"3","title":"Екатеринбург"},{"id":"3","title":"Челябинск"}]
+        },
+    },
+
 };
 </script>
